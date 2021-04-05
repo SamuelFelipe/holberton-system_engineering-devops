e07823e41f7566c9992d28c3d747419efce3538f
SamuelFelipe <samuelfelipecorrea@gmail.com>
2021-04-05 07:29:05 -0500

    Upload avanced task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# configuration with puppet
+file_line { 'Authentication off':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'IdentityFile':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
+}
