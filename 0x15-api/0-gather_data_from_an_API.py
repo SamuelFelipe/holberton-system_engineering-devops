b00ac5a665f38a1079ab1397b6c320f8c6a51eb1
Samuel Felipe <2354@holbertonschool.com>
2021-05-03 18:18:36 -0500

    Update - No execute when imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,30 +6,30 @@ https://jsonplaceholder.typicode.com/todos
 and show the response
 '''
 
-
-from sys import argv
-import json
-import requests
-
-
-user_url = 'https://jsonplaceholder.typicode.com/users/{}'
-task_url = 'https://jsonplaceholder.typicode.com/users/{}/todos/'
-user_id = argv[1]
-out = 'Employee {} is done with task({}/{}):{}'
+if __name__ == '__main__':
+    from sys import argv
+    import json
+    import requests
 
 
-responce_user = requests.get(user_url.format(user_id))
-responce_task = requests.get(task_url.format(user_id))
-completed = 0
-
-info_task = json.loads(responce_task.text)
-info_user = json.loads(responce_user.text)
-titles = ''
-
-for i in info_task:
-    if i['completed']:
-        completed += 1
-        titles += '\n\t ' + i['title']
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'
+    task_url = 'https://jsonplaceholder.typicode.com/users/{}/todos/'
+    user_id = argv[1]
+    out = 'Employee {} is done with task({}/{}):{}'
 
 
-print(out.format(info_user['name'], completed, len(info_task), titles))
+    responce_user = requests.get(user_url.format(user_id))
+    responce_task = requests.get(task_url.format(user_id))
+    completed = 0
+
+    info_task = json.loads(responce_task.text)
+    info_user = json.loads(responce_user.text)
+    titles = ''
+
+    for i in info_task:
+        if i['completed']:
+            completed += 1
+            titles += '\n\t ' + i['title']
+
+
+    print(out.format(info_user['name'], completed, len(info_task), titles))
