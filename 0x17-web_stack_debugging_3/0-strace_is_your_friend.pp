5e5a7c5a3889590393a0f07e30be0ad0dbd6c5ce
SamuelFelipe <2354@holbertonschool.com>
2021-05-19 12:51:17 -0500

    Update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { 'change':
-  path    => '/usr/local/bin'
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
 }
