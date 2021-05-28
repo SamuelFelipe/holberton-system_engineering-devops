08e7438c8ed9158f7ff29e9c39fd232d043210a9
SamuelFelipe <2354@holbertonschool.com>
2021-05-27 19:39:14 -0500

    Update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+# solve a problem
 exec { 'change':
   path    =>   '/usr/bin/env:/bin/',
   command =>   'sed -i s/15/1000/ /etc/default/nginx',
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,4 @@
+# solve a problem
 exec {
   path    =>    '/usr/bin/env:/bin/',
   command =>    'sed -i "s/holberton/foo/" /etc/security/limits.conf',
