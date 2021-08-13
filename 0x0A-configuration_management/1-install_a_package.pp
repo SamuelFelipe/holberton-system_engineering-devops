c66e4e9b777200b58acd09abd19f261f982d7654
SamuelFelipe <2354@holbertonschool.com>
2021-08-13 10:26:52 -0500

    Remove

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# install puppet-lint version 2.1.1
-package {'puppet-lint':
-  ensure  => '2.1.1',
-  provide => 'gem'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# kill a program
-exec {
-  command => 'pkill killmenow',
-  path    => '/usr/bin'
-}
