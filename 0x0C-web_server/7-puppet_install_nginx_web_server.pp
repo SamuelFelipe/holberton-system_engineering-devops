a7bcfa77bb4090f6646d5ed253a68f0895a4cc39
SamuelFelipe <2354@holbertonschool.com>
2021-08-17 19:22:31 -0500

    Update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,21 @@
 # install and configure nginx
 
+$cmd = @("Command"/L)
+  new_string="\n\n\tlocation \/redirect_me {\n\t\treturn 301 Holberton School;\n\t}\n"
+  ALREADY_IN=$( grep "redirect_me" /etc/nginx/sites-available/default)
+
+  if [[ -z $ALREADY_IN ]]; then
+    sudo sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;$new_string/" /etc/nginx/sites-available/default
+    sudo service nginx restart
+  fi
+ | - Command
+
 exec {
   command => 'sudo apt-get update; sudo apt-get install nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
 exec {
-  command => 'new_string="\n\n\tlocation \/redirect_me {\n\t\treturn 301 Holberton School;\n\t}\n"
-ALREADY_IN=$( grep "redirect_me" /etc/nginx/sites-available/default)
-
-if [[ -z $ALREADY_IN ]]; then
-  sudo sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;$new_string/" /etc/nginx/sites-available/default
-  sudo service nginx restart
-fi'
+  command =>   
   path    => '/usr/local/bin/:/bin/'
 }
