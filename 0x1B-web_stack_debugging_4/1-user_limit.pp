1a43ad330f3a24f74b2a12d1f94ac93a8cce06ed
SamuelFelipe <2354@holbertonschool.com>
2021-10-07 20:17:05 -0500

    Update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,11 @@
 # solve a problem
 exec {
-  path    =>    '/usr/bin:/bin',
-  command =>    'sed -i "s/5/50000/" /etc/security/limits.conf; sed -i "s/4/40000/" /etc/security/limits.conf',
+  environment => ['DIR=/etc/security/limits.conf',
+                  'OLD=hard nofile 5',
+                  'NEW=hard nofile 50000',
+                  'OLD2=soft nofile 4',
+                  'NEW2=soft nofile 40000'],
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo sed -i "s/$OLD2/$NEW2/" $DIR',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1]
 }
