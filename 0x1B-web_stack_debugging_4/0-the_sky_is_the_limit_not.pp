06dc438f9c4d86c72fd425e59e36524d8e967bd5
SamuelFelipe <2354@holbertonschool.com>
2021-10-07 20:00:15 -0500

    Updae

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # solve a problem
 exec { 'change':
   path    =>   '/usr/bin/env:/bin/',
-  command =>   'sed -i s/15/1000/ /etc/default/nginx',
+  command =>   'sed -i s/15/10000/ /etc/default/nginx',
 }
